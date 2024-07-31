bd1f96a9414a06d21f6b4599db7dc1cba7413035
KayisHlm <kayishilmi24@gmail.com>
2024-07-31 22:41:07 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('ANALISIS DATA PENYEWAAN SEPEDA')
 st.write('###### Pada kesempatan kali ini, saya akan menyajikan hasil',
